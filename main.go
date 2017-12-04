4e36f12dd2872fd616504963a9cb624975ca010d
rmulton <rmulton@gmail.com>
2017-12-04 09:36:49 +0100

    Refactored

--- main.go
+++ main.go
@@ -15,15 +15,15 @@ type document struct {
 	partList []part
 }
 
-func parseDocument (filePath string) []document {
+func parseDocuments (filePath string) []document {
 	dat, err := ioutil.ReadFile(filePath)
-
-	var output []document
-
+	
 	if err != nil {
 		panic(err)
 	}
-
+	
+	var output []document
+	
 	fileString := string(dat) // Change this to handle bigger files
 
 	// Split the files in documents
@@ -31,25 +31,10 @@ func parseDocument (filePath string) []document {
 	docs := regexDoc.Split(fileString, -1)
 	docsNum := regexDoc.FindAllStringSubmatch(fileString, -1)
 	
-	var partList []part
 	for i, doc := range docs {
-		if i>0 {
-			// Split the doc in parts
-			regexDocPart := regexp.MustCompile("\\.([A-Z])\n")
-			partsContent := regexDocPart.Split(doc, -1)
-			partsName := regexDocPart.FindAllStringSubmatch(doc, -1)
-	
-	
-			// Add part to doc data structure
-			fmt.Println("Document:", docsNum[i-1][1])
-			for j, partName := range partsName {
-				partName := partName[1]
-				partContent := partsContent[j+1]
-				part := part{partName, partContent}
-				partList = append(partList, part)
-			}
-	
+		if doc != "" {
 			// Create doc data structure
+			partList := parseDocument(doc)	
 			parsedDoc := document{docsNum[i-1][1], partList}
 			output = append(output, parsedDoc)
 		}
@@ -57,8 +42,26 @@ func parseDocument (filePath string) []document {
 	return output
 }
 
+func parseDocument(doc string) []part {
+	var partList []part
+	// Split the doc in parts
+	regexDocPart := regexp.MustCompile("\\.([A-Z])\n")
+	partsContent := regexDocPart.Split(doc, -1)
+	partsName := regexDocPart.FindAllStringSubmatch(doc, -1)
+	
+	// Add part to doc data structure
+	for j, partName := range partsName {
+		partName := partName[1]
+		partContent := partsContent[j+1]
+		part := part{partName, partContent}
+		partList = append(partList, part)
+	}
+	
+	return partList
+}
+
 func main() {
 	filePath := "./consignes/Data/CACM/cacm.all"
-	parsedDoc := parseDocument(filePath)
+	parsedDoc := parseDocuments(filePath)
 	fmt.Println("Done", parsedDoc[0])
 }
