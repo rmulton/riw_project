2bcdfb717655e4215eabbe3c90745148c167f7a7
rmulton <rmulton@gmail.com>
2017-12-04 11:48:59 +0100

    moved stoplist file to global file variable

--- main.go
+++ main.go
@@ -2,70 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"regexp"
+	"./reader/parser/cacm"
 )
 
-type part struct {
-	category string
-	content string
-}
-type document struct {
-	id string
-	partList []part
-}
-
-func parseDocuments (filePath string) []document {
-	// Create output variable
-	var output []document
-	
-	// Read the file
-	dat, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		panic(err)
-	}
-	fileString := string(dat) // Change this to handle bigger files
-
-	// Split the files in documents
-	regexDoc := regexp.MustCompile(".I ([0-9]*)\n")
-	docs := regexDoc.Split(fileString, -1)
-	docsNum := regexDoc.FindAllStringSubmatch(fileString, -1)
-	
-	// Iterate over the documents and parse them
-	for i, doc := range docs {
-		if doc != "" {
-			// Create the document data structure
-			partList := parseDocument(doc)	
-			parsedDoc := document{docsNum[i-1][1], partList}
-			output = append(output, parsedDoc)
-		}
-	}
-
-	return output
-}
-
-func parseDocument(doc string) []part {
-	// Create output variable
-	var partList []part
-
-	// Split the doc in parts
-	regexDocPart := regexp.MustCompile("\\.([A-Z])\n")
-	partsContent := regexDocPart.Split(doc, -1)
-	partsName := regexDocPart.FindAllStringSubmatch(doc, -1)
-	
-	// Add part to doc data structure
-	for j, partName := range partsName {
-		partName := partName[1]
-		partContent := partsContent[j+1]
-		part := part{partName, partContent}
-		partList = append(partList, part)
-	}
-	
-	return partList
-}
-
 func main() {
-	filePath := "./consignes/Data/CACM/cacm.all"
-	parsedDocs := parseDocuments(filePath)
-	fmt.Println("Done", parsedDocs[0])
+	filePath := "./consignes/Data/CACM/"
+	parsedDocs := cacm.ParseDocuments(filePath)
+	fmt.Println("Done", parsedDocs[62])
 }
---
+++ reader/parser/cacm/cacm.go
@@ -0,0 +1,128 @@
+package cacm
+
+import (
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"strconv"
+)
+
+type part struct {
+	category string
+	tokens []string
+}
+
+// Document : Structure to store a document
+type Document struct {
+	id int
+	partList []part
+}
+// Read stop words file
+var folderPath = "./consignes/Data/CACM/"
+var stopListFile = fileToString(folderPath + "common_words")
+var stopList = strings.Split(stopListFile, "\n")
+
+// ParseDocuments : Parse CACM documents
+func ParseDocuments(folderPath string) []Document {
+	// Create output variable
+	var documents []Document
+	// var reversedIndex map[string]map[string]int
+
+	
+		
+	// Read documents file
+	fileString := fileToString(folderPath + "cacm.all") // Change this to handle bigger files
+
+	// Split the files in documents
+	regexDoc := regexp.MustCompile(".I ([0-9]*)\n")
+	docs := regexDoc.Split(fileString, -1)
+	docsNum := regexDoc.FindAllStringSubmatch(fileString, -1)
+	
+	// Iterate over the documents and parse them
+	for i, doc := range docs {
+		if doc != "" {
+			// Create the document data structure
+			partList := parseDocument(doc)	
+			docID, err := strconv.Atoi(docsNum[i-1][1])
+			if err == nil {
+				parsedDoc := Document{docID, partList}
+				documents = append(documents, parsedDoc)
+			}
+		}
+	}
+
+	return documents
+}
+
+func parsePart(content string) []string {
+	// Create output variable
+	var loweredTokens []string
+
+	// Split content into tokens
+	tokens := strings.FieldsFunc(content, func(r rune) bool {
+		return r == ' ' || r == '.' || r == '\n' || r == ','
+	})
+
+	// Tokens to lower case
+	for _, token := range tokens {
+		lowered := strings.ToLower(token)
+		loweredTokens = append(loweredTokens, lowered)
+		loweredTokens = removeUnsignificantTokens(loweredTokens)
+	}
+
+	return loweredTokens
+}
+
+func fileToString(filePath string) string {
+	dat, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	fileString := string(dat)
+	return fileString
+}
+
+func removeUnsignificantTokens(tokens []string) []string {
+	// Create output variable
+	var significantTokens []string
+
+	// Get significant words
+	for _, token := range tokens {
+		if isSignificant(token) {
+			significantTokens = append(significantTokens, token)
+		}
+	}
+
+	return significantTokens
+}
+
+func isSignificant(token string) bool {
+	// TODO : inefficient
+	for _, unsignificantWord := range stopList {
+		if token == unsignificantWord {
+			return false
+		}
+	}
+	return true
+}
+
+func parseDocument(doc string) []part {
+	// Create output variable
+	var partList []part
+
+	// Split the doc in parts
+	regexDocPart := regexp.MustCompile("\\.([A-Z])\n")
+	partsContent := regexDocPart.Split(doc, -1)
+	partsName := regexDocPart.FindAllStringSubmatch(doc, -1)
+	
+	// Add part to doc data structure
+	for j, partName := range partsName {
+		partName := partName[1]
+		partContent := partsContent[j+1]
+		partTokens := parsePart(partContent)
+		part := part{partName, partTokens}
+		partList = append(partList, part)
+	}
+	
+	return partList
+}
