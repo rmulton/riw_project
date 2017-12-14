8061e910b0059b50b27b31a1e7fddd1b0a1754f7
rmulton <rmulton@gmail.com>
2017-12-14 15:56:42 +0100

    Removed old useless files

--- parsers/cacm/Document.go
+++
@@ -1,7 +0,0 @@
-package cacm
-
-// Document : Structure to store a document
-type Document struct {
-	id int
-	partList []part
-}
--- parsers/cacm/part.go
+++
@@ -1,6 +0,0 @@
-package cacm
-
-type part struct {
-	category string
-	tokens []string
-}
