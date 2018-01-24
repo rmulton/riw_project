15e009c7a0c6daa6e61f099fb3d1c71de15617ce
rmulton <rmulton@gmail.com>
2018-01-24 08:33:54 +0100

    better test version for the main function

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ import (
 	"bufio"
 	"os"
 	"fmt"
+	// "./indexes"
 	"./requests"
 )
 
 func readInput() string {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("\n>> Please enter request: ")
+	fmt.Print(">> Please enter a request: ")
 	text, _ := reader.ReadString('\n')
 	return text
 }
@@ -22,9 +23,14 @@ func readInput() string {
 func main() {
 	start := time.Now()
 	var waitGroup sync.WaitGroup
-	reader := readers.NewStanfordReader(
-		"../consignes/Data/CS276/pa1-data/",
-		10,
+	// reader := readers.NewStanfordReader(
+	// 	"../consignes/Data/CS276/pa1-data/",
+	// 	10,
+	// 	&waitGroup,
+	// )
+	reader := readers.NewCACMReader(
+		"../consignes/Data/cacm/",
+		5,
 		&waitGroup,
 	)
 	builder := indexBuilders.NewInMemoryBuilder(
@@ -32,6 +38,13 @@ func main() {
 		10,
 		&waitGroup,
 	)
+	// builder := indexBuilders.NewOnDiskBuilder(
+	// 	1000000000,
+	// 	"./saved",
+	// 	reader.Docs,
+	// 	10,
+	// 	&waitGroup,
+	// )
 	log.Println("Starting")
 	waitGroup.Add(2)
 	go reader.Read()
@@ -41,6 +54,8 @@ func main() {
 	elapsed := done.Sub(start)
 	log.Printf("Done in %v", elapsed)
 	index := builder.GetIndex()
+	fmt.Printf("%#v", index.GetPostingListsForTerms([]string{"stanford"}))
+	// index := indexes.OnDiskIndexFromFolder("./saved/")
 	engine := requests.NewEngine(index)
 	var req string
 	for {
