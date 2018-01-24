d001c7f3a8a58143d6d6791aeba9d4dc0716bae7
rmulton <rmulton@gmail.com>
2018-01-24 08:33:19 +0100

    Added switches to the engine

--- requests/Engine.go
+++ requests/Engine.go
@@ -10,10 +10,23 @@ type Engine struct {
 	outputFormater outputFormater
 }
 
-func NewEngine(index indexes.RequestableIndex) *Engine {
-	// index := IndexFromFolder(folder)
-	requestHandler := NewVectorizedRequestHandler(index)
-	outputFormater := NewSortDocsOutputFormater()
+func NewEngine(index indexes.RequestableIndex, requestType string, outputType string) *Engine {
+	var requestHandler requestHandler
+	switch requestType {
+	case "and":
+		requestHandler = NewAndRequestHandler(index)
+	case "binary":
+		requestHandler = NewBinaryRequestHandler(index)
+	case "vectorial":
+		requestHandler = NewVectorizedRequestHandler(index)
+	}
+	var outputFormater outputFormater
+	switch outputType {
+	case "sorted":
+		outputFormater = NewSortDocsOutputFormater("../consignes/Data/CACM/")
+	case "dumb":
+		outputFormater = NewDumbOutputFormater()
+	}
 	return &Engine{
 		index: index,
 		requestHandler: requestHandler,
