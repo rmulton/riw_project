1837aaa553415abff82974a66879f569462e0357
rmulton <rmulton@gmail.com>
2018-02-18 16:24:50 +0100

    broken imports

--- indexes/Index.go
+++ indexes/Index.go
@@ -83,11 +83,9 @@ func (index *Index) AddDocToIndex(docID int, docPath string) {
 
 func (index *Index) ClearPostingListFor(term string) {
 	delete(index.postingLists, term)
-	// TODO: decide which one is the more efficient
-	// emptyPostingList := make(PostingList)
-	// index.postingLists[term] = emptyPostingList
 }
 
 func (index *Index) GetDocCounter() int {
-	return len(index.docIDToFilePath)
+	fmt.Println(len(index.docIDToFilePath))
+	return 10
 }
